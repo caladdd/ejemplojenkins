dc21fb03f9a5735eaa05800d67fa6bb060657df4
caladdd <juanpablo.calad@gmail.com>
2018-11-01 07:27:36 -0500

    error python manage

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     a = 1
-     b = 2
+     b = "2"
+     c = 1 - b
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
